55c30a2bfbb2c6f247985dad1f49529ac2f83210
Rachel <lhearachel@proton.me>
2024-01-31 21:08:12 -0800

    [neovim] refactor nvim-lspconfig entry

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -38,31 +38,31 @@ return {
     },
     {
         'neovim/nvim-lspconfig',
+        opts = {
+            servers = {
+                clangd = {},
+                cmake = {},
+                dockerls = {},
+                docker_compose_language_service = {},
+                gradle_ls = {},
+                jsonls = {},
+                jdtls = {},
+                kotlin_language_server = {},
+                ltex = {},
+                lua_ls = {},
+                autotools_ls = {},
+                marksman = {},
+                swift_mesonls = {},
+                jedi_language_server = {},
+                rust_analyzer = {},
+                taplo = {},
+                terraformls = {},
+                tsserver = {},
+                lemminx = {},
+                yamlls = {},
+            }
+        },
         config = function()
-            local config = require('lspconfig')
-            -- config.bashls.setup({})  -- weird nvim_buf_del_keymap error?
-            config.clangd.setup({})
-            -- config.csharp_ls.setup({}) -- needs dotnet exe
-            config.cmake.setup({})
-            config.dockerls.setup({})
-            config.docker_compose_language_service.setup({})
-            config.gradle_ls.setup({})
-            config.jsonls.setup({})
-            config.jdtls.setup({})
-            config.kotlin_language_server.setup({})
-            config.ltex.setup({})
-            config.lua_ls.setup({})
-            config.autotools_ls.setup({})
-            config.marksman.setup({})
-            config.swift_mesonls.setup({})
-            config.jedi_language_server.setup({})
-            config.rust_analyzer.setup({})
-            config.taplo.setup({})
-            config.terraformls.setup({})
-            config.tsserver.setup({})
-            config.lemminx.setup({})
-            config.yamlls.setup({})
-
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
