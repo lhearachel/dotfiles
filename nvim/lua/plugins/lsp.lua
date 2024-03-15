88295c80ed99bf40c584b3edbe7a9511d63c42a9
Rachel <lhearachel@proton.me>
2024-03-15 11:06:15 -0700

    [neovim] Add C# LSP

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,6 +5,7 @@ return {
       ensure_installed = {
         "bash-language-server",
         "clangd",
+        "csharp-language-server",
         "dockerfile-language-server",
         "jedi-language-server",
         "json-lsp",
