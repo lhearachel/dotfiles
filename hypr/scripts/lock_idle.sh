4886ade1f6b92f823c040147edf0677fe17cc296
Rachel <lhearachel@proton.me>
2024-12-13 21:02:29 -0800

    cleanup: Sweep away old configuration

--- hypr/scripts/lock_idle.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-swayidle -w timeout 600 'swaylock -f' timeout 660 'hyperctl dispatch dpms off' resume 'hyperctl dispatch dpms on'
--- nvim/lua/plugins/disable.lua
+++
@@ -1,4 +0,0 @@
-return {
-  { "rcarriga/nvim-notify", enabled = false },
-  { "echasnovski/mini.indentscope", enabled = false },
-}
