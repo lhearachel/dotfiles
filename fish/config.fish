c465ce47df7cc6a3c6d289e43a06371849d7d385
Rachel <lhearachel@proton.me>
2024-12-15 14:18:50 -0800

    feat(fish): Setup user path

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,22 @@
-set -g XDG_CONFIG_HOME "$HOME/.config"
-set -g XDG_DATA_HOME "$HOME/.local/share"
-set -g XDG_STATE_HOME "$HOME/.local/state"
-set -g XDG_BIN_HOME "$HOME/.local/bin"
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_STATE_HOME "$HOME/.local/state"
+set -gx XDG_BIN_HOME "$HOME/.local/bin"
 
-set -g BROWSER firefox
-set -g VISUAL nvim
-set -g EDITOR nvim
+set -gx BROWSER firefox
+set -gx VISUAL nvim
+set -gx EDITOR nvim
+
+set -gx DOTFILES "$HOME/dotfiles"
+set -gx PYENV_ROOT "$HOME/.pyenv"
+set -gx CARGO_ROOT "$HOME/.cargo"
+
+fish_add_path "$XDG_BIN_HOME"
+fish_add_path "$DOTFILES/scripts"
+fish_add_path "$PYENV_ROOT/bin"
+fish_add_path "$CARGO_ROOT/bin"
 
 if status is-interactive
     fish_config theme choose catppuccin
+    pyenv init - | source
 end
