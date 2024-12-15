60f5c8547c2331bac3d732825b1752494f615f0d
Rachel <lhearachel@proton.me>
2024-12-15 15:29:07 -0800

    fix(fish): Fix ef, ev, sf functions

--- fish/functions/ef.fish
+++ fish/functions/ef.fish
@@ -1,5 +1,5 @@
 function ef --description="Edit fish configuration"
-    pushd /fish >/dev/null
+    pushd $__fish_config_dir >/dev/null
     v config.fish
     popd - >/dev/null
 end
--- fish/functions/ev.fish
+++ fish/functions/ev.fish
@@ -1,5 +1,5 @@
 function ev --description="Edit nvim configuration"
-    pushd $XDG_CONFIG_HOME/nvim >/dev/null
+    pushd "$XDG_CONFIG_HOME/nvim" >/dev/null
     v init.lua
     popd - >/dev/null
 end
