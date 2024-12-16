d36958c01f52fd2dd34548571c07b68b99ffeb2c
Rachel Forshee <rachel.forshee@elastic.co>
2024-12-16 13:28:50 -0800

    feat(fish): Add Java version to prompt context

--- fish/functions/_prompt_context.fish
+++ fish/functions/_prompt_context.fish
@@ -12,9 +12,10 @@ function _prompt_context \
     set --local prompt_python (_prompt_context_python)
     set --local prompt_node (_prompt_context_node)
     set --local prompt_ruby (_prompt_context_ruby)
+    set --local prompt_java (_prompt_context_java)
 
     set --local prompt
-    for elem in $prompt_ssh $prompt_cwd $prompt_ruby $prompt_python $prompt_node $prompt_git $prompt_err
+    for elem in $prompt_ssh $prompt_cwd $prompt_ruby $prompt_python $prompt_node $prompt_java $prompt_git $prompt_err
         if test (string length $elem) -gt 0
             set --append prompt "$elem"
         end
