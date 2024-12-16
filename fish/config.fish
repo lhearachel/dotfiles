de7368f162498d45766f7d2fda8893f2e8824cc2
Rachel <lhearachel@proton.me>
2024-12-15 20:34:43 -0800

    fix(fish): Add homebrew binaries to path on Mac

--- fish/config.fish
+++ fish/config.fish
@@ -25,6 +25,10 @@ fish_add_path "$DOTFILES/scripts"
 fish_add_path "$PYENV_ROOT/bin"
 fish_add_path "$CARGO_ROOT/bin"
 
+if test (uname -s) = "Darwin"
+    fish_add_path "/opt/homebrew/bin"
+end
+
 set -gx fish_prompt_pwd_dir_length 0
 
 if status is-interactive
