05ab3e40eb6205e0cb391438256fb7040b483c54
Rachel <lhearachel@proton.me>
2025-01-04 11:38:06 -0800

    feat(fish): Add rfad-local configuration for hledger

--- fish/config_rfad.fish
+++ fish/config_rfad.fish
@@ -2,3 +2,6 @@ set -gx CLIP_COPY wl-copy
 
 # License file for building pokeplatinum
 set -gx LM_LICENSE_FILE "$GIT_REPOS/pokeplatinum/tools/cw/license.dat"
+
+# Environment variables used by hledger
+set -gx LEDGER_FILE ~/finance/journals/2025/2025.journal
