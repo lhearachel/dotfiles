71792a8eb67c45d679fa65e206880a9f729e6c91
Rachel <lhearachel@proton.me>
2025-01-10 16:28:51 -0800

    fix(fish): Add ruby gems to path

--- fish/config_rfad.fish
+++ fish/config_rfad.fish
@@ -5,3 +5,5 @@ set -gx LM_LICENSE_FILE "$GIT_REPOS/pokeplatinum/tools/cw/license.dat"
 
 # Environment variables used by hledger
 set -gx LEDGER_FILE ~/finance/journals/2025/2025.journal
+
+fish_add_path "$XDG_DATA_HOME/gem/ruby/3.3.0/bin"
