f9b8beadd34ae0b890c2d8acd665c86dc15e0294
Rachel <lhearachel@proton.me>
2025-02-21 22:23:54 -0800

    feat: Updates from rfembp

--- fish/config_rfembp.fish
+++ fish/config_rfembp.fish
@@ -9,8 +9,19 @@ set -gx GIT_REPOS "$CODE_REPOS/github.com"
 set -gx MY_GIT_REPOS "$CODE_REPOS/github.com"
 set -gx DEX "$MY_GIT_REPOS/dex"
 
+# My upstream username is different from my local user.
+set -gx CHEF_USER rforshee
+set -gx ST_USER rforshee
+
 if status is-interactive
     # I typically only use Ruby and Java in a working environment.
     type -q --no-functions rbenv; and rbenv init - | source
     type -q --no-functions jenv; and jenv init - | source
 end
+
+senv gitlab_pat
+senv github_pat
+senv vault.infra
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '~/google-cloud-sdk/path.fish.inc' ]; . '~/google-cloud-sdk/path.fish.inc'; end
