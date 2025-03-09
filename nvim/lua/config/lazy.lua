b02942d41a5cdfe269f9aafe310fe61f8d7beca2
Rachel <lhearachel@proton.me>
2025-03-09 12:50:14 -0700

    feat(nvim): Enable snacks explorer and picker

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -27,6 +27,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.illuminate" },
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
     { import = "lazyvim.plugins.extras.editor.navic" },
+    { import = "lazyvim.plugins.extras.editor.snacks_explorer" },
+    { import = "lazyvim.plugins.extras.editor.snacks_picker" },
 
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.git" },
