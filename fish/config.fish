ddb3b88ab4279d4051c88723534ddbec23b5fc99
Rachel <lhearachel@proton.me>
2025-03-11 11:33:19 -0700

    feat: Integrate mise for tool-versioning

--- fish/config.fish
+++ fish/config.fish
@@ -37,6 +37,9 @@ set -gx fish_prompt_pwd_dir_length 0
 if status is-interactive
     fish_config theme choose catppuccin
     type -q --no-functions pyenv; and pyenv init - fish | source
+    type -q --no-functions mise; and mise activate --shims fish | source
+else
+    type -q --no-functions mise; and mise activate fish | source
 end
 
 function _space_prompts --on-event fish_postexec
