536ceb53dd581d6ed68fd4ed4ebb693ed24316be
Rachel <lhearachel@proton.me>
2025-05-03 13:10:52 -0700

    feat(nvim): Disable auto-formatting

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.autoformat = false
 vim.g.snacks_animate = false
 vim.g.lazyvim_picker = "auto"
 vim.g.lazyvim_cmp = "auto"
