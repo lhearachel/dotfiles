8f476c638d0b0ae861d089be56c3258c1107c92c
Rachel <lhearachel@proton.me>
2025-06-04 13:58:22 -0700

    feat(nvim): Enable rust language add-ons

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -36,6 +36,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.ruby" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.tex" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
