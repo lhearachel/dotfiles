d4d7530297548513fd02934ff058ab9ddec64bf0
Rachel <lhearachel@proton.me>
2025-06-16 19:12:50 -0700

    feat(nvim): Re-enable SchemaStore

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -2,10 +2,6 @@ return {
   -- lazyvim.plugins.extras.lang.git
   { "petertriho/cmp-git", enabled = false },
 
-  -- lazyvim.plugins.extras.lang.json
-  -- lazyvim.plugins.extras.lang.yaml
-  { "b0o/SchemaStore.nvim", enabled = false },
-
   -- lazyvim.plugins.extras.lang.ruby
   { "olimorris/neotest-rspec", enabled = false },
   { "suketa/nvim-dap-ruby", enabled = false },
