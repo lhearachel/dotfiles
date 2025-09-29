4d1783b2653abb6723bba469b9ffeda4c6a82295
Rachel <lhearachel@proton.me>
2025-09-29 09:32:40 -0700

    feat(nvim): Add keybind to create daily journal entries

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+require("which-key").add({
+  "<leader>fj",
+  "<cmd>!./journal/newdaily.sh<CR>",
+  desc = "New Daily Entry",
+  icon = { icon = "󰈔", color = "cyan" },
+})
