91b7587d42fc61b2deaf05787e1c01dd95c2c5bf
Rachel <lhearachel@proton.me>
2025-11-06 11:10:54 -0800

    feat(fish): Use asdf on MacOS

--- fish/config_rfembp.fish
+++ fish/config_rfembp.fish
@@ -23,5 +23,19 @@ set -gx SDKROOT (xcrun --show-sdk-path)
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '~/google-cloud-sdk/path.fish.inc' ]
-    . '~/google-cloud-sdk/path.fish.inc'
+    source '~/google-cloud-sdk/path.fish.inc'
 end
+
+# ASDF configuration code
+if test -z "$ASDF_DATA_DIR"
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
