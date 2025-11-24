e46c34644f589671032d1ec5b6ba071fb3d876a5
Rachel <rachel@lhea.me>
2025-11-23 21:29:26 -0800

    fix(nvim): Remove lingering asciidoc reference

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -34,6 +34,7 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.git" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.ruby" },
--- nvim/lua/plugins/lang/asciidoc.lua
+++
@@ -1,5 +0,0 @@
-return {
-  {
-    "cathaysia/tree-sitter-asciidoc"
-  },
-}
