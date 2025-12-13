5dced056a7d22dce07586b960e6fb5c9109313e6
Rachel <rachel@lhea.me>
2025-12-13 15:02:37 -0800

    chore(nvim): Remove unused tex plugin-pack

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -38,7 +38,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.ruby" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
-    { import = "lazyvim.plugins.extras.lang.tex" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
 
