6f39bc27b1dc662d929e1d73e9817bf918d2a8bf
Rachel <rachel@lhea.me>
2025-12-13 15:02:04 -0800

    fix(nvim): Remove mini-diff to fix random nvim crashes

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -28,8 +28,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.dial" },
     { import = "lazyvim.plugins.extras.editor.illuminate" },
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
-    { import = "lazyvim.plugins.extras.editor.mini-diff" },
-    -- { import = "lazyvim.plugins.extras.editor.snacks_explorer" },
     { import = "lazyvim.plugins.extras.editor.snacks_picker" },
 
     { import = "lazyvim.plugins.extras.lang.clangd" },
