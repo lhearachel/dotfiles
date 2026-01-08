8c2b96514c9c4412e93134da9b0a7b45e1306064
Rachel <lhearachel@proton.me>
2026-01-08 10:50:18 -0800

    chore(fish): Tidy up rfembp config and add some TODOs

--- fish/config_rfembp.fish
+++ fish/config_rfembp.fish
@@ -4,8 +4,10 @@ fish_add_path /opt/homebrew/sbin
 fish_add_path /opt/homebrew/opt/bison/bin
 set -gx BROWSER "open -a firefox"
 set -gx CLIP_COPY pbcopy
+set -gx HOMEBREW_USE_INTERNAL_API 1
 
 # This Macbook maintains siloing of Git repositories by GitHub user.
+# TODO: I want to change this in the future.
 set -gx GIT_REPOS "$CODE_REPOS/github.com"
 set -gx MY_GIT_REPOS "$CODE_REPOS/github.com"
 set -gx DEX "$MY_GIT_REPOS/dex"
@@ -14,6 +16,8 @@ set -gx DEX "$MY_GIT_REPOS/dex"
 set -gx CHEF_USER rforshee
 set -gx ST_USER rforshee
 
+# Load some important access-tokens.
+# TODO: Move this to a global direnv config.
 senv gitlab_pat
 senv github_pat
 senv vault.infra
@@ -39,6 +43,3 @@ if not contains $_asdf_shims $PATH
     set -gx --prepend PATH $_asdf_shims
 end
 set --erase _asdf_shims
-
-# Enable Homebrew's internal JSON API
-set -gx HOMEBREW_USE_INTERNAL_API 1
